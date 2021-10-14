0a7066db7b27533530dd021ef1c356f66c3fce03
Zemiro014 <zemiro014@gmail.com>
2021-10-13 22:18:55 -0300

    Project with Headercomponent and using tailwind created

---
+++ instagram-clone/components/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Image from "next/image";
+
+function Header() {
+  return (
+    <div>
+      <div className="flex justify-between max-w-6xl">
+        {/* Left */}
+        <div className="relative hidden lg:inline-grid h-24 w-24">
+          <Image src="https://links.papareact.com/ocw" layout="fill"  objectFit="contain"/>
+        </div>
+        <div className="relative w-10 h-10 lg:hidden flex-shrink-0">
+        <Image src="https://links.papareact.com/jjm" layout="fill"  objectFit="contain"/>
+        </div>
+        {/* Midle */}
+
+        {/* Right */}
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ instagram-clone/pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import Header from '../components/Header'
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Instagram 2.0 in youtube</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {/* Headers */}
+      <Header />
+
+      {/* Feed */}
+
+      {/* Modal */}
+    </div>
+  )
+}
