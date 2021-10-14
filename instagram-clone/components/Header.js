4276c932d859bc06c5de11c215aec2106f6d1439
Zemiro014 <zemiro014@gmail.com>
2021-10-13 22:38:54 -0300

    components, heroicon and taiwindcss

--- instagram-clone/components/Header.js
+++ instagram-clone/components/Header.js
@@ -1,5 +1,13 @@
 import React from "react";
 import Image from "next/image";
+import {SearchIcon,
+  PlusCircleIcon,
+  UserGroupIcon,
+  HeartIcon,
+  PaperAirplaneIcon,
+  Menucon,
+
+} from "@heroicons/react/outline";
 
 function Header() {
   return (
@@ -9,10 +17,16 @@ function Header() {
         <div className="relative hidden lg:inline-grid h-24 w-24">
           <Image src="https://links.papareact.com/ocw" layout="fill"  objectFit="contain"/>
         </div>
-        <div className="relative w-10 h-10 lg:hidden flex-shrink-0">
+        <div className="relative w-10 h-10 lg:hidden flex-shrink-0 cursor-pointer">
         <Image src="https://links.papareact.com/jjm" layout="fill"  objectFit="contain"/>
         </div>
-        {/* Midle */}
+        {/* Midle - search input field*/}
+        <div>
+          <div>
+            <SearchIcon  className='h-5 w-5 text-gray-500'/>
+          </div>
+          <input type="text" placeholder="Search"/>
+        </div>
 
         {/* Right */}
       </div>
