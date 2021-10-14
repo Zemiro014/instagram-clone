2796b0ba80b496f1e79ac2b15ee608168fbb15f0
Zemiro014 <zemiro014@gmail.com>
2021-10-13 23:22:49 -0300

    Global Css file with tailwindcss

--- instagram-clone/components/Header.js
+++ instagram-clone/components/Header.js
@@ -1,34 +1,53 @@
 import React from "react";
 import Image from "next/image";
-import {SearchIcon,
+import {
+  SearchIcon,
   PlusCircleIcon,
   UserGroupIcon,
   HeartIcon,
   PaperAirplaneIcon,
-  Menucon,
-
+  MenuIcon,
+  HomeIcon,
 } from "@heroicons/react/outline";
 
 function Header() {
   return (
     <div>
-      <div className="flex justify-between max-w-6xl">
+      <div className="flex justify-between max-w-6xl mx-5 xl:mx-auto">
         {/* Left */}
-        <div className="relative hidden lg:inline-grid h-24 w-24">
-          <Image src="https://links.papareact.com/ocw" layout="fill"  objectFit="contain"/>
+        <div className="relative hidden lg:inline-grid w-24">
+          <Image
+            src="https://links.papareact.com/ocw"
+            layout="fill"
+            objectFit="contain"
+          />
         </div>
-        <div className="relative w-10 h-10 lg:hidden flex-shrink-0 cursor-pointer">
-        <Image src="https://links.papareact.com/jjm" layout="fill"  objectFit="contain"/>
+        <div className="relative w-10 lg:hidden flex-shrink-0 cursor-pointer">
+          <Image
+            src="https://links.papareact.com/jjm"
+            layout="fill"
+            objectFit="contain"
+          />
         </div>
         {/* Midle - search input field*/}
-        <div>
-          <div>
-            <SearchIcon  className='h-5 w-5 text-gray-500'/>
+        <div className="max-w-sm">
+          <div className=" relative mt-1 p-3 rounded-md">
+            <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+              <SearchIcon className="h-5 w-5 text-gray-500" />
+            </div>
+            <input
+              className="bg-gray-50 block w-full pl-10 sn:text-sm border-gray focus:ring-black focus:border-black rounded-md"
+              type="text"
+              placeholder="Search"
+            />
           </div>
-          <input type="text" placeholder="Search"/>
         </div>
-
         {/* Right */}
+        <div className="flex items-center justify-end space-x-4">
+          <HomeIcon className="navBtn" />
+          <HomeIcon className="h-6 md:hidden cursor-pointer" />
+          <PaperAirplaneIcon className="navBtn"/>
+        </div>
       </div>
     </div>
   );
